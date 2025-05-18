86d28deeee18031351830d9292f25807c615d59e
soukaina.morghati <morghatisoukaina1@gmail.com>
2025-05-18 11:11:53 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'atelier5';
+  user: string[] = ['soukaina', 'said', 'abdelaknir', 'mouad'];
+  nouveauNom: string = '';
+
+  ajouter(nom: string) {
+    if (nom.trim()) {
+      this.user.push(nom.trim());
+      this.nouveauNom = '';
+    }
+  }
+
+  supprimer() {
+    this.user.pop();
+  }
+
+  vider() {
+    this.user = [];
+  }
 }
